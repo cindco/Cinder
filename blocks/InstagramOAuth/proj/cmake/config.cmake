171594eb24ccd0c7311de3c24a97cb9616edff42
Bruno Herfst <bruno.herfst@gmail.com>
2025-07-09 16:25:33 +1000

    No more external API shit. They should go in external blocks

--- blocks/InstagramOAuth/proj/cmake/config.cmake
+++
@@ -1,7 +0,0 @@
-# InstagramOAuth block configuration
-# Set includes and libraries for the InstagramOAuth block
-
-set(InstagramOAuth_INCLUDES "${CMAKE_CURRENT_LIST_DIR}/../../include" CACHE STRING "InstagramOAuth include path")
-
-# The InstagramOAuth library is built as part of the block's CMakeLists.txt
-# and linked automatically when the block is used 
