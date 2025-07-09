d0592f34616d0c49905a37343ad70b7f88df91c0
Bruno Herfst <bruno.herfst@gmail.com>
2025-07-09 14:33:47 +1000

    Try and use the new twitter API

--- samples/_svg/GoodNightMorning/src/TweetStream.h
+++ samples/_svg/GoodNightMorning/src/TweetStream.h
@@ -3,9 +3,16 @@
 #include "cinder/Surface.h"
 #include "cinder/ConcurrentCircularBuffer.h"
 #include "cinder/Thread.h"
-#include "cinder/Json.h"
 
 #include <string>
+
+// X API (Twitter API v2) Configuration
+// To use the real X API, you need to:
+// 1. Go to https://developer.twitter.com/
+// 2. Create a developer account and app
+// 3. Get your Bearer Token
+// 4. Set TWITTER_BEARER_TOKEN in TweetStream.cpp
+// Note: The X API has rate limits and requires authentication
 
 class Tweet {
   public:
